0a07bcc0dd4d859334b268f25e681f2f35b8f59c
matteao <matteao_03@163.com>
2020-02-22 21:49:12 +0800

    add auth method

--- src/TokenGuard.php
+++ src/TokenGuard.php
@@ -3,6 +3,8 @@
 namespace Matteao\TokenGuard;
 
 use \Illuminate\Auth\SessionGuard;
+use Illuminate\Contracts\Auth\Authenticatable as AuthenticatableContract;
+use Illuminate\Support\Facades\Cache;
 
 class TokenGuard extends SessionGuard
 {
@@ -21,20 +23,55 @@ class TokenGuard extends SessionGuard
             return $this->user;
         }
 
-        $user = null;
+        $id = Cache::get($this->getName());
 
-        $id = $this->session->get($this->getName());
 
         if (!is_null($id) && $this->user = $this->provider->retrieveById($id)) {
+            $this->updateCache($this->getName(), $id); //更新cache
+
             $this->fireAuthenticatedEvent($this->user);
         }
 
-        return $this->user = $user;
+        return $this->user;
+    }
+
+    public function attempt(array $credentials = [], $remember = false)
+    {
+        $this->fireAttemptEvent($credentials, $remember);
+
+        $this->lastAttempted = $user = $this->provider->retrieveByCredentials($credentials);
+
+        if ($this->hasValidCredentials($user, $credentials)) {
+            $sessionId = $this->login($user, $remember);
+
+            return $sessionId;
+        }
+
+        $this->fireFailedEvent($user, $credentials);
+
+        return false;
+    }
+
+    public function login(AuthenticatableContract $user, $remember = false)
+    {
+        $sessionId = $this->session->getId();
+        $this->updateCache($sessionId, $user->getAuthIdentifier());
+
+        $this->fireLoginEvent($user, $remember);
+
+        $this->setUser($user);
+
+        return $sessionId;
+    }
+
+    protected function updateCache($key, $id)
+    {
+        Cache::put($key, $id, 3600 * 24);
     }
 
     protected function clearUserDataFromStorage()
     {
-        $this->session->remove($this->getName());
+        Cache::forget($this->getName());
     }
 
     /**
@@ -44,6 +81,6 @@ class TokenGuard extends SessionGuard
      */
     public function getName()
     {
-        return  $this->request->bearerToken();
+        return  $this->request->bearerToken() ?: '';
     }
 }
